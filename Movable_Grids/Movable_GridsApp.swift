960148fd0cf40f8f6e388fb7e999f365b77d4417
Valerii Gassiev <vvv.gassiev@gmail.com>
2023-10-17 21:30:02 +0300

    Initial Commit

---
+++ Movable_Grids/Movable_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movable_GridsApp.swift
+//  Movable_Grids
+//
+//  Created by Valera Gassiev on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Movable_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movable_GridsUITests/Movable_GridsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movable_GridsUITestsLaunchTests.swift
+//  Movable_GridsUITests
+//
+//  Created by Valera Gassiev on 10/17/23.
+//
+
+import XCTest
+
+final class Movable_GridsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
